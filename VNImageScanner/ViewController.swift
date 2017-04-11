d2d4a0db0314a2fa1f3a676bd6e65a29b0fed308
Varun Naharia <varunnaharia@varun.local>
2017-04-11 16:50:30 +0530

    Initial Commit

---
+++ VNImageScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VNImageScanner
+//
+//  Created by Varun Naharia on 11/04/17.
+//  Copyright © 2017 Varun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
